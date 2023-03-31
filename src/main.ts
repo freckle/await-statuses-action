a2c99313b1e7453f3077591aaef3ac5d6fd4685c
patrick brisbin <pbrisbin@gmail.com>
2023-03-31 12:17:10 -0400

    First implementation

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,85 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+import { getInputs } from "./inputs";
+import type { Statuses } from "./status";
+import { checkRunsToStatuses } from "./status";
+import { listCheckRunsForRef } from "./check-run";
 
 async function run() {
   try {
+    const { ref, statusNames, pollSeconds, pollLimit, githubToken } =
+      getInputs();
 
-    core.startGroup("Inputs");
-    const token = core.getInput("github-token", { required: true });
-    core.info("Action loaded")
+    const prRef = github.context.payload.pull_request?.head.ref;
+    const shaRef = github.context.sha;
+    const statusRef = ref !== null && ref.trim() !== "" ? ref : prRef ?? shaRef;
 
+    if (!statusRef) {
+      throw new Error(
+        `Unable to determine ref. Input: ${ref}, PullRequest: ${prRef}, SHA: ${shaRef}`
+      );
+    }
+
+    core.info(`Awaiting ${statusNames.length} statuse(s) on ${statusRef}`);
+
+    const client = github.getOctokit(githubToken);
+
+    for (let i = 1; true; i++) {
+      if (i > pollLimit) {
+        throw new Error(`Poll limit reached (${i} > ${pollLimit})`);
+      }
+
+      const checkRuns = await listCheckRunsForRef(client, statusRef);
+      const statuses = await checkRunsToStatuses(checkRuns, statusNames);
+
+      logStatuses(statuses);
+
+      if (requirementsMet(statuses)) {
+        core.info("All statuses successful");
+        return;
+      }
+
+      core.info("Some statuses are still pending");
+      core.info(`Polling again in ${pollSeconds} second(s)`);
+      await sleep(pollSeconds);
+    }
   } catch (error) {
     if (error instanceof Error) {
-      core.error(error);
       core.setFailed(error.message);
     } else if (typeof error === "string") {
-      core.error(error);
       core.setFailed(error);
     } else {
-      core.error("Non-Error exception");
       core.setFailed("Non-Error exception");
     }
   }
+}
+
+function logStatuses({ pending, succeeded, failed }: Statuses): void {
+  succeeded.sort().forEach((s) => {
+    core.info(`\u001b[32m✓\u001b[0m ${s} has \u001b[32msucceeded\u001b[0m`);
+  });
+
+  failed.sort().forEach((s) => {
+    core.info(`\u001b[31mx\u001b[0m ${s} has \u001b[31mfailed\u001b[0m`);
+  });
+
+  pending.sort().forEach((s) => {
+    core.info(`  ${s} is \u001b[33mpending\u001b[0m`);
+  });
+}
+
+function requirementsMet({ pending, failed }: Statuses): boolean {
+  if (failed.length > 0) {
+    throw new Error(`Some required statuses have failed: ${failed.join(", ")}`);
+  }
+
+  return pending.length === 0;
+}
+
+function sleep(seconds: number) {
+  const ms = seconds * 1000;
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 run();
---
+++ src/status.ts
@@ -0,0 +1,76 @@
+import * as github from "@actions/github";
+import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+
+import type { GitHubClient, CheckRun } from "./check-run";
+import { listCheckRunsForRef } from "./check-run";
+
+const SUCCESS_CONCLUSIONS = ["neutral", "skipped", "success"];
+const FAILURE_CONCLUSIONS = [
+  "action_required",
+  "cancelled",
+  "failure",
+  "stale",
+  "startup_failure",
+  "timed_out",
+];
+
+export type Statuses = {
+  pending: string[];
+  succeeded: string[];
+  failed: string[];
+};
+
+export function checkRunsToStatuses(
+  checkRuns: CheckRun[],
+  statusNames: string[]
+): Statuses {
+  const all: string[] = [];
+  const pending: string[] = [];
+  const succeeded: string[] = [];
+  const failed: string[] = [];
+
+  checkRuns.forEach((run) => {
+    if (!includes(statusNames, run.name)) {
+      return; // Not required, don't care
+    }
+
+    all.push(run.name);
+
+    if (run.conclusion === null) {
+      return pending.push(run.name);
+    }
+
+    if (includes(SUCCESS_CONCLUSIONS, run.conclusion)) {
+      return succeeded.push(run.name);
+    }
+
+    if (includes(FAILURE_CONCLUSIONS, run.conclusion)) {
+      return failed.push(run.name);
+    }
+
+    throw new Error(
+      `Unexpected CheckRun conclusion: ${
+        run.conclusion
+      }. Must be one of ${SUCCESS_CONCLUSIONS.concat(FAILURE_CONCLUSIONS).join(
+        ", "
+      )}`
+    );
+  });
+
+  // Add any statuses that we didn't see at all as pending
+  statusNames.forEach((name) => {
+    if (!includes(all, name)) {
+      pending.push(name);
+    }
+  });
+
+  return {
+    pending,
+    failed,
+    succeeded,
+  };
+}
+
+function includes<T>(xs: Array<T>, x: T): boolean {
+  return xs.indexOf(x) !== -1;
+}
