d3c9245ab3bdf9bb08b9193f21f5213bf3a958d8
patrick brisbin <pbrisbin@gmail.com>
2023-05-16 11:44:12 -0400

    Introduce brief output format
    The default behavior should be the same.

--- src/inputs.ts
+++ src/inputs.ts
@@ -1,10 +1,13 @@
 import * as core from "@actions/core";
 
+export type Format = "rich" | "brief";
+
 export type Inputs = {
   ref: string | null;
   statusNames: string[];
   pollSeconds: number;
   pollLimit: number;
+  format: Format;
   githubToken: string;
 };
 
@@ -14,6 +17,7 @@ export function getInputs(): Inputs {
     statusNames: core.getMultilineInput("statuses", { required: true }),
     pollSeconds: getInputNum("poll-seconds"),
     pollLimit: getInputNum("poll-limit"),
+    format: core.getInput("format", { required: true }) as Format,
     githubToken: getInput("github-token"),
   };
 }
