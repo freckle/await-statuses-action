3241a30b1756ba74947ca9b11a6fc8937c7ddf18
patrick brisbin <pbrisbin@gmail.com>
2023-07-13 14:52:13 -0400

    Support for optional statuses
    As per the README,
    
        Elements in the `statuses` list may optionally be suffixed by any
        number of spaces and a `?`. This makes it acceptable if they never
        appear, but will still consider it a failure if they do appear as
        failed. This can be useful for matrix jobs whose statuses never
        appear in certain skipped scenarios, but we still want to account
        for them if they fail.

--- src/status.test.ts
+++ src/status.test.ts
@@ -13,3 +13,16 @@ test("For an example ref on megarepo", async () => {
   expect(statuses.succeeded).toEqual(["backend / test", "test-sql"]);
   expect(statuses.failed).toEqual(["platform-frontend / platform-image"]);
 });
+
+test("With optional statuses", async () => {
+  const statuses = checkRunsToStatuses(TEST_CHECK_RUNS, [
+    "backend / test",
+    "platform-frontend / platform-image?",
+    "test-sql",
+    "unknown-status ?",
+  ]);
+
+  // not waiting on unknown-status, but still failing on platform-image
+  expect(statuses.pending).toEqual([]);
+  expect(statuses.failed).toEqual(["platform-frontend / platform-image"]);
+});
